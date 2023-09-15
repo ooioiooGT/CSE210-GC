00f5a39c23b0fdf41cccdcf1e3d260fca2e3961b
Gilber Chen <gilber1002@byui.edu>
2023-09-15 16:56:54 -0600

    prep2 ;

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grad persentage? ");
+        string gcInput = Console.ReadLine();
+        int gcGrad = int.Parse(gcInput);
+
+        string gcLetter = "";
+        
+        if (gcGrad >= 90)
+        {
+            gcLetter = "A";
+        }else if (gcGrad >= 80)
+        {
+            gcLetter = "B";
+        }else if (gcGrad >= 70)
+        {
+            gcLetter = "C";
+        }else if (gcGrad >= 60)
+        {
+            gcLetter = "D";
+        }else 
+        {
+            gcLetter = "F";
+        }
+        Console.WriteLine($"Your grad is {gcLetter}");
+
+        if (gcGrad >= 70)
+        {
+            Console.WriteLine("You pass!");
+        }else
+        {
+            Console.WriteLine("I believed you will do better next time");
+        }
+
     }
 }
