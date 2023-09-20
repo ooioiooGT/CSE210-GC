4e4549b2c343f5d78eabbd7b2b510cd9416ffcf4
Gilber Chen <gilber1002@byui.edu>
2023-09-19 20:45:50 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine ("What is your magic number?");
+        // string gcInput = Console.ReadLine();
+        // int gcMagicNumber = int.Parse(gcInput);
+        Random gcRandom = new Random();
+        int gcRNumber = gcRandom.Next(1,101);
+        int gcGus = 0 ;
+        int gcCount = 0;
+        bool gcAgain = true;
+
+        while (gcAgain)
+        {
+            while( gcRNumber != gcGus)
+            {
+                Console.WriteLine("What do you guess?");
+                string gcIn = Console.ReadLine();
+                gcGus = int.Parse(gcIn);
+                gcCount ++ ;
+                if (gcGus > gcRNumber){
+                    Console.WriteLine("please guess again, lower!");
+                }else if (gcGus < gcRNumber){
+                    Console.WriteLine("Please guess again, higher!");
+                }else{
+                    Console.WriteLine("You got it");
+                }
+            }
+            Console.WriteLine($"you guess {gcCount} times");
+
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string gcAgainInput = Console.ReadLine().ToLower();
+            if (gcAgainInput != "yes"){
+                gcAgain = false;
+            }
+        }
+
     }
 }
