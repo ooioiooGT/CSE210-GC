8073f7821186704500a123a16adc6839e23eec33
Gilber Chen <gilber1002@byui.edu>
2023-09-21 17:18:01 -0600

    finised pep4

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        gcDisplayWelcome();
+        string gcNa = gcPromptUserName();
+        int gcN = gcPromptUserNumber();
+        int gcNu = gcSquareNumber(gcN);
+        gcDisplayResult(gcNa, gcNu);
+    }
+    static void gcDisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string gcPromptUserName(){
+        Console.WriteLine("Please enter your name:");
+        string gcName = Console.ReadLine();
+        // Console.WriteLine($"{gcName}");
+        return gcName;
+    }
+    static int gcPromptUserNumber(){
+        Console.WriteLine("Please enter your favorite number:");
+        string gcIn = Console.ReadLine();
+        int gcNumber = int.Parse(gcIn);
+        return gcNumber;
+    }
+    static int gcSquareNumber(int gcNumber){
+        int gcSquare = gcNumber * gcNumber ;
+        return gcSquare;
+    }
+    static void gcDisplayResult(string gcName ,int gcSquare){
+        Console.WriteLine($"{gcName}, the square of your number is {gcSquare}");
     }
 }
