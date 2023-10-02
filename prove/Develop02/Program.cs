2bf606cdb0b239d490b2faa4e756e21c3565d2df
Gilber Chen <gilber1002@byui.edu>
2023-10-02 12:26:23 -0600

    set up

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,44 @@
+
+/* 
+
+Name: Digital Journal
+Purpose: Records prompts from user
+Author(s): Gilber Chen, Ty Saltzgiver, Brian Ritchie, Bradley Roberts
+Date: (c) 2023
+
+*/
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Choose an option: \n 1. write a journal  \n 2. Save prompts  \n 3. Load prompt  \n 4. End Program  \n 5. Get Input  \n 6. Display Input ");
+        string userInPut = Console.ReadLine();
+        if (userInPut == "1"){
+            
+        }
+
     }
+    public class Getrandom{
+        Random gcRandom = new Random();
+    }
+    public class UserInputJournal{
+       string journalresponse = Console.ReadLine();
+       
+    }
+
+    public class Display{
+
+    }
+
+    public class SaveInput{
+
+    }
+
+    public class Loadinputs{
+
+    }
+
 }
+    
